2483cebf610fed88c0e35664602f7bd00083565a
dhaydt <1992dayat@gmail.com>
2022-11-02 08:56:51 +0700

    rm dd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -48,7 +48,8 @@ class Controller extends BaseController
         $jasperstarter = base_path('/vendor/cossou/jasperphp/src/JasperStarter/lib/jasperstarter.jar');
         $parameter = 'mulai="'.$date.'" spt_id='.$id.' tanggal_naskah="'.$naskah.'"';
         $database = 'mysql -H localhost -u c1_etask -p KhSh_Bx4 -n c1_etask';
-        dd("java -jar $jasperstarter pr $input -o $output -f docx -P $parameter");
+
+        // dd("java -jar $jasperstarter pr $input -o $output -f docx -P $parameter");
         exec("java -jar $jasperstarter pr $input -o $output -f docx -P $parameter -t $database");
 
         $remove = sptGenerate::where('spt_id', $id)->get();
