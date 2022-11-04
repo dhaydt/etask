d6f546908cea07201fcbbc29a77ca2ec37036d78
dhaydt <1992dayat@gmail.com>
2022-11-04 08:53:09 +0700

    browser agent

--- app/Http/Controllers/AutentikasController.php
+++ app/Http/Controllers/AutentikasController.php
@@ -38,7 +38,7 @@ class AutentikasController extends Controller
             $token = Helpers::crypt(date('Y-m-d H:i:s'));
 
             $agent = Helpers::regexUserAgent($request->header('user-agent'));
-            $version = $agent['browser'].' '.$agent['version'] ? $agent['browser'].' '.$agent['version'] : 'Android';
+            $version = $agent['browser'] ? $agent['browser'].' '.$agent['version'] : 'Android';
 
             $loginLogs = LoginLogs::create([
                 'nip_user' => $user['nip'],
