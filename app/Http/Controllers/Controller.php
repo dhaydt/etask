3c0e7c83a1f17f3b742a0e20f43e510e0e6a8ae8
dhaydt <1992dayat@gmail.com>
2022-11-08 15:14:48 +0700

    memeritahkan hiden n

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -92,7 +92,7 @@ class Controller extends BaseController
             $spt->spt_id = $id;
             $spt->save();
         }
-        $input = public_path('js/SPT23.jrxml');
+        $input = public_path('js/SPT24.jrxml');
         $output = public_path('/storage/spt');
 
         $dasar = '';
@@ -118,7 +118,7 @@ class Controller extends BaseController
 
         $this->removeTask($id);
 
-        return response()->file(public_path('storage/spt/SPT23.docx'));
+        return response()->file(public_path('storage/spt/SPT24.docx'));
     }
 
     public function removeTask($id)
