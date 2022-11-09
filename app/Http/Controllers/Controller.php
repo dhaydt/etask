9052b8eb76b72717807f72e9557a17d98b2e131e
dhaydt <1992dayat@gmail.com>
2022-11-09 16:25:10 +0700

    fix header

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -111,7 +111,7 @@ class Controller extends BaseController
             $spt->save();
         }
         if (count($staff) > 3) {
-            $input = public_path('template/SPTTable5.jrxml');
+            $input = public_path('template/SPTTable6.jrxml');
         } else {
             $input = public_path('template/SPT33.jrxml');
         }
@@ -145,7 +145,7 @@ class Controller extends BaseController
         $this->removeTask($id);
 
         if (count($staff) > 3) {
-            return response()->file(public_path('storage/spt/SPTTable5.docx'));
+            return response()->file(public_path('storage/spt/SPTTable6.docx'));
         } else {
             return response()->file(public_path('storage/spt/SPT33.docx'));
         }
