67b38e6f32d8167f0bfc78754c00b304e095ee99
dhaydt <1992dayat@gmail.com>
2022-11-09 17:00:19 +0700

    field height

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -111,7 +111,7 @@ class Controller extends BaseController
             $spt->save();
         }
         if (count($staff) > 3) {
-            $input = public_path('template/SPTTable8.jrxml');
+            $input = public_path('template/SPTTable9.jrxml');
         } else {
             $input = public_path('template/SPT33.jrxml');
         }
@@ -145,7 +145,7 @@ class Controller extends BaseController
         $this->removeTask($id);
 
         if (count($staff) > 3) {
-            return response()->file(public_path('storage/spt/SPTTable8.docx'));
+            return response()->file(public_path('storage/spt/SPTTable9.docx'));
         } else {
             return response()->file(public_path('storage/spt/SPT33.docx'));
         }
