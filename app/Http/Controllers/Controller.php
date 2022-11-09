986f08a68a9efd0d32ffcfd918c2d8cdc6d750ca
dhaydt <1992dayat@gmail.com>
2022-11-09 12:11:20 +0700

    fix template

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -105,7 +105,7 @@ class Controller extends BaseController
             $spt->spt_id = $id;
             $spt->save();
         }
-        $input = public_path('template/SPT32.jrxml');
+        $input = public_path('template/SPT33.jrxml');
         $output = public_path('/storage/spt');
 
         $dasar = '';
@@ -131,7 +131,7 @@ class Controller extends BaseController
 
         $this->removeTask($id);
 
-        return response()->file(public_path('storage/spt/SPT32.docx'));
+        return response()->file(public_path('storage/spt/SPT33.docx'));
     }
 
     public function removeTask($id)
