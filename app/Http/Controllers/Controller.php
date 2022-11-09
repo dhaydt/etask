d791199134f31f8ba394c592542b70d722092087
dhaydt <1992dayat@gmail.com>
2022-11-09 12:06:06 +0700

    fix template jasper

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -105,7 +105,7 @@ class Controller extends BaseController
             $spt->spt_id = $id;
             $spt->save();
         }
-        $input = public_path('template/SPT30.jrxml');
+        $input = public_path('template/SPT31.jrxml');
         $output = public_path('/storage/spt');
 
         $dasar = '';
@@ -131,7 +131,7 @@ class Controller extends BaseController
 
         $this->removeTask($id);
 
-        return response()->file(public_path('storage/spt/SPT30.docx'));
+        return response()->file(public_path('storage/spt/SPT31.docx'));
     }
 
     public function removeTask($id)
