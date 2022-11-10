64d1a3a06b629faed4e7e149082585034f1d1031
dhaydt <1992dayat@gmail.com>
2022-11-10 08:16:32 +0700

    err spt

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -111,7 +111,7 @@ class Controller extends BaseController
             $spt->save();
         }
         if (count($staff) > 3) {
-            $input = public_path('template/SPTTable8.jrxml');
+            $input = public_path('template/SPTTable11.jrxml');
         } else {
             $input = public_path('template/SPT33.jrxml');
         }
@@ -145,7 +145,7 @@ class Controller extends BaseController
         $this->removeTask($id);
 
         if (count($staff) > 3) {
-            return response()->file(public_path('storage/spt/SPTTable8.docx'));
+            return response()->file(public_path('storage/spt/SPTTable11.docx'));
         } else {
             return response()->file(public_path('storage/spt/SPT33.docx'));
         }
