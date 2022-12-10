bf2eef8b448baa03febcc338103327bd81a1507e
dhaydt <1992dayat@gmail.com>
2022-12-11 03:15:29 +0700

    etask spt template

--- app/Http/Controllers/GenerateController.php
+++ app/Http/Controllers/GenerateController.php
@@ -108,7 +108,7 @@ class GenerateController extends Controller
         if (count($staff) > 3) {
             $input = public_path('template/SPTTable39.jrxml');
         } else {
-            $input = public_path('template/SPT10.jrxml');
+            $input = public_path('template/SPT19.jrxml');
         }
         $output = public_path('/storage/spt');
 
@@ -142,7 +142,7 @@ class GenerateController extends Controller
         if (count($staff) > 3) {
             return response()->file(public_path('storage/spt/SPTTable39.docx'));
         } else {
-            return response()->file(public_path('storage/spt/SPT10.docx'));
+            return response()->file(public_path('storage/spt/SPT19.docx'));
         }
     }
 }
