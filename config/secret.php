3a05ee4aaa1e4abffef91bd5bbf77d0d1a36bafb
dhaydt <1992dayat@gmail.com>
2024-04-03 10:08:23 +0700

    add config file

---
+++ config/secret.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+    'e-task' => '821b9ca567ec95bdef466799278277af',
+    'presensi' => '09b6e75ad96057a3e78f4b8d8d22aa4f',
+];
